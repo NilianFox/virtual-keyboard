b75196db6378d00b612ac1899db6b107b59afc15
Danil Garmashov <wizzy_fox@mail.ru>
2023-05-01 15:23:56 +0700

    feat: add basic keyboard generation ans some files refactor

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// eslint-disable-next-line import/extensions
+import keysLayout from './keys.js';
+
+const Keyboard = {
+  elements: {
+    main: null,
+    keys: [],
+  },
+
+  properties: {
+    language: 'rus',
+    value: '',
+    capsLock: false,
+  },
+
+  init() {
+    // Create main element
+    this.elements.main = document.createElement('div');
+    this.elements.main.classList.add('keyboard');
+    this.elements.main.id = 'keyboard';
+    this.elements.main.innerHTML = this.createKeys();
+
+    // Add to DOM
+    document.body.append(this.elements.main);
+  },
+
+  createKeys() {
+    let fragment = '';
+    keysLayout.forEach((element) => {
+      fragment += '<div class="keyboard__row">';
+      element.forEach((key) => {
+        if (key.size) {
+          fragment += `<div data-key-code="${key.code}" style="--key-size: ${key.size}" data-key-display="${key.key}" class="key"></div>`;
+        } else if (this.properties.language === 'eng') {
+          fragment += `<div data-key-code="${key.code}" data-key-display="${key.key}" class="key"></div>`;
+        } else if (this.properties.language === 'rus') {
+          fragment += `<div data-key-code="${key.code}" data-key-display="${key.keyRu}" class="key"></div>`;
+        }
+      });
+      fragment += '</div>';
+    });
+    return fragment;
+  },
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  Keyboard.init();
+});
+
+window.onkeydown = (e) => {
+  console.log(document.querySelector(`[data-key-code="${e.code}"]`), e);
+  document.querySelector(`[data-key-code="${e.code}"]`).classList.add('active');
+};
+
+window.onkeyup = (e) => {
+  document.querySelector(`[data-key-code="${e.code}"]`).classList.remove('active');
+};
